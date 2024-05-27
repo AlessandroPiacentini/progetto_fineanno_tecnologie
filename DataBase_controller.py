4694c18a6e0961e07c040eed09136e0911968402
AlessandroPiacentini <you@example.compiacentinialessandro05@gmail.com>
2024-05-27 11:31:39 +0200

    chore: Add "Back home" link to add_spese.html and spese.html templates

--- DataBase_controller.py
+++ DataBase_controller.py
@@ -2,18 +2,22 @@ import mysql.connector
 
 class Database:
     _instance = None
-    _conn = None
 
     def __init__(self):
         if not Database._instance:
             Database._instance = self
             self._dbname = "gestione_spese"
-            self._conn = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="",
-                database=self._dbname
-            )
+            try:
+                self._conn = mysql.connector.connect(
+                    host="localhost",
+                    user="root",
+                    password="",
+                    database=self._dbname
+                )
+            except mysql.connector.Error as err:
+                print(f"Errore di connessione al database: {err}")
+                raise
+
         else:
             raise Exception("Non puoi creare un'altra istanza di questa classe.")
 
@@ -24,67 +28,96 @@ class Database:
         return Database._instance
 
     def updateTable(self, table, fields, where=None):
-        sql = f"UPDATE {table} SET "
-        params = []
-        for field, value in fields.items():
-            sql += f"{field} = %s, "
-            params.append(value)
-        sql = sql.rstrip(', ')
-        if where is not None:
-            sql += " WHERE "
-            for field, value in where.items():
-                sql += f"{field} = %s AND "
+        try:
+            sql = f"UPDATE {table} SET "
+            params = []
+            for field, value in fields.items():
+                sql += f"{field} = %s, "
                 params.append(value)
-            sql = sql.rstrip(" AND ")
-        cur = self._conn.cursor()
-        cur.execute(sql, params)
-        self._conn.commit()
+            sql = sql.rstrip(', ')
+            if where is not None:
+                sql += " WHERE "
+                for field, value in where.items():
+                    sql += f"{field} = %s AND "
+                    params.append(value)
+                sql = sql.rstrip(" AND ")
+            cur = self._conn.cursor()
+            cur.execute(sql, params)
+            self._conn.commit()
+        except mysql.connector.Error as err:
+            print(f"Errore durante l'aggiornamento della tabella: {err}")
+            raise
 
     def execute_query(self, sql):
-        cur = self._conn.cursor()
-        cur.execute(sql)
-        self._conn.commit()
-        return cur.fetchall()
+        try:
+            cur = self._conn.cursor()
+            cur.execute(sql)
+            self._conn.commit()
+            return cur.fetchall()
+        except mysql.connector.Error as err:
+            print(f"Errore durante l'esecuzione della query: {err}")
+            raise
+        finally:
+            cur.close()
 
     def read_table(self, table, where=None):
-        sql = f"SELECT * FROM {table}"
-        params = None
-        if where is not None:
-            sql += " WHERE "
+        try:
+            sql = f"SELECT * FROM {table}"
+            params = None
+            if where is not None:
+                sql += " WHERE "
+                params = []
+                for field, value in where.items():
+                    sql += f"{field} = %s AND "
+                    params.append(value)
+                sql = sql.rstrip(" AND ")
+            cur = self._conn.cursor()
+            if params is not None:
+                cur.execute(sql, params)
+            else:
+                cur.execute(sql)
+            self._conn.commit()
+            return cur.fetchall()
+        except mysql.connector.Error as err:
+            print(f"Errore durante la lettura della tabella: {err}")
+            raise
+        finally:
+            cur.close()
+
+    def delete(self, table, where):
+        try:
+            sql = f"DELETE FROM {table} WHERE "
             params = []
             for field, value in where.items():
                 sql += f"{field} = %s AND "
                 params.append(value)
             sql = sql.rstrip(" AND ")
-        cur = self._conn.cursor()
-        if params is not None:
+            cur = self._conn.cursor()
             cur.execute(sql, params)
-        else:
-            cur.execute(sql)
-        return cur.fetchall()
-
-    def delete(self, table, where):
-        sql = f"DELETE FROM {table} WHERE "
-        params = []
-        for field, value in where.items():
-            sql += f"{field} = %s AND "
-            params.append(value)
-        sql = sql.rstrip(" AND ")
-        cur = self._conn.cursor()
-        cur.execute(sql, params)
-        self._conn.commit()
+            self._conn.commit()
+        except mysql.connector.Error as err:
+            print(f"Errore durante l'eliminazione dei dati: {err}")
+            raise
+        finally:
+            cur.close()
 
     def insert(self, table, data):
-        sql = f"INSERT INTO {table} ("
-        values = " VALUES ("
-        params = []
-        for field, value in data.items():
-            sql += f"{field}, "
-            values += "%s, "
-            params.append(value)
-        sql = sql.rstrip(', ') + ")"
-        values = values.rstrip(', ') + ")"
-        sql += values
-        cur = self._conn.cursor()
-        cur.execute(sql, params)
-        self._conn.commit()
+        try:
+            sql = f"INSERT INTO {table} ("
+            values = " VALUES ("
+            params = []
+            for field, value in data.items():
+                sql += f"{field}, "
+                values += "%s, "
+                params.append(value)
+            sql = sql.rstrip(', ') + ")"
+            values = values.rstrip(', ') + ")"
+            sql += values
+            cur = self._conn.cursor()
+            cur.execute(sql, params)
+            self._conn.commit()
+        except mysql.connector.Error as err:
+            print(f"Errore durante l'inserimento dei dati: {err}")
+            raise
+        finally:
+            cur.close()
--- spese_controller.py
+++ spese_controller.py
@@ -15,6 +15,11 @@ class SpeseController:
         def get_spese():
             return self.retrieve_spese()
         
+        
+        @self.app.route('/get_spese_mese')
+        def get_spese_mese():
+            return self.retrieve_spese_mese()
+        
         @self.app.route('/add_spesa', methods=['POST'])
         def add_spesa():
             titolo = request.form.get('titolo')
@@ -39,4 +44,9 @@ class SpeseController:
     def retrieve_spese(self):
         where = {"id_user": session['id']}
         result = self.db.read_table('spese', where=where)
+        return jsonify(result)
+    
+    def retrieve_spese_mese(self):
+        where = {"id_user": session['id']}
+        result = self.db.execute_query('SELECT * FROM spese WHERE month(data) = month(now())')
         return jsonify(result)
